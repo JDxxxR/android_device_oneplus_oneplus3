eef5032481e122482af91713cacc4d3d014ecb09
jdxxxr <at660404@gmail.com>
2020-07-16 15:15:11 +0000

    remove telephony.mk from TWRP tree

--- lineage.mk
+++ lineage.mk
@@ -15,7 +15,9 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# semms that the build fails in minimal manifest, unless its commented out
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
